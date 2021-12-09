3924c1514fccb9928ad28d47947924f68b9c1547
Aaron Faanes <dafrito@gmail.com>
2021-12-09 02:11:18 -0600

    Upgrade webpack library name

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist-prod"),
     filename: "parsegraph-camera.js",
     globalObject: "this",
-    library: "parsegraph",
+    library: "parsegraph_camera",
     libraryTarget: "umd",
   },
   module: {
