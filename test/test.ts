906fed2e76092cae0fb2ff03e12b673072b26f43
Aaron Faanes <dafrito@gmail.com>
2022-02-26 19:34:23 -0600

    Fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,57 +1,20 @@
-var assert = require("assert");
-import Camera, { containsAny, containsAll } from "../dist/camera";
-
-import TestSuite from "parsegraph-testsuite";
+const {assert} = require("chai");
+import { containsAny, containsAll } from "../src/index";
 
 describe("Camera", function () {
-  it("works", () => {
-    const cameraTests = new TestSuite("Camera");
+  it("containsAll", ()=>{
+    assert.ok(containsAll(0, 0, 800, 600, 0, 0, 400, 200), "Small box in viewport");
+    assert.ok(containsAll(0, 0, 800, 600, 0, 0, 900, 200), "Taller box in viewport");
+    assert.ok(containsAll(0, 0, 800, 600, 0, 0, 400, 1000), "Wider box in viewport");
+    assert.ok(containsAll(0, 0, 800, 600, 0, 0, 1000, 1000), "Larger box in viewport");
+    assert.ok(containsAll(0, 0, 800, 600, 600, 0, 400, 200), "Small box on edge of viewport");
+  });
 
-    cameraTests.addTest("containsAll", function () {
-      if (!containsAll(0, 0, 800, 600, 0, 0, 400, 200)) {
-        return "Small box in viewport";
-      }
-
-      if (containsAll(0, 0, 800, 600, 0, 0, 900, 200)) {
-        return "Taller box in viewport";
-      }
-
-      if (containsAll(0, 0, 800, 600, 0, 0, 400, 1000)) {
-        return "Wider box in viewport";
-      }
-
-      if (containsAll(0, 0, 800, 600, 0, 0, 1000, 1000)) {
-        return "Larger box in viewport";
-      }
-
-      if (containsAll(0, 0, 800, 600, 600, 0, 400, 200)) {
-        return "Small box on edge of viewport";
-      }
-    });
-
-    cameraTests.addTest("containsAny", function () {
-      if (!containsAny(0, 0, 800, 600, 0, 0, 400, 200)) {
-        return "Small box in viewport";
-      }
-
-      if (!containsAny(0, 0, 800, 600, 0, 0, 900, 200)) {
-        return "Taller box in viewport";
-      }
-
-      if (!containsAny(0, 0, 800, 600, 0, 0, 400, 1000)) {
-        return "Wider box in viewport";
-      }
-
-      if (!containsAny(0, 0, 800, 600, 0, 0, 1000, 1000)) {
-        return "Larger box in viewport";
-      }
-
-      if (!containsAny(0, 0, 800, 600, 600, 0, 400, 200)) {
-        return "Small box on edge of viewport";
-      }
-    });
-
-    const result = cameraTests.run();
-    assert.ok(result.isSuccessful());
+  it("containsAny", ()=>{
+    assert.ok(containsAny(0, 0, 800, 600, 0, 0, 400, 200), "Small box in viewport");
+    assert.ok(containsAny(0, 0, 800, 600, 0, 0, 900, 200), "Taller box in viewport");
+    assert.ok(containsAny(0, 0, 800, 600, 0, 0, 400, 1000), "Wider box in viewport");
+    assert.ok(containsAny(0, 0, 800, 600, 0, 0, 1000, 1000), "Larger box in viewport");
+    assert.ok(containsAny(0, 0, 800, 600, 600, 0, 400, 200), "Small box on edge of viewport");
   });
 });
